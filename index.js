a12999297450474884aaa43e4d462fb440e87af6
coltenappleby <colten.appleby@gmail.com>
2021-04-09 13:43:44 -0400

    done; added listeners for both inputs

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
+function addingEventListener(input) {
+
+    
+
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+    });
+
 }
+
+const input1 = document.querySelectorAll('input')[0];
+const input2 = document.querySelectorAll('input')[1];
+addingEventListener(input1)
+addingEventListener(input2)
+
