bba32d388628a474bb2058fbf1e47ca262739f48
coltenappleby <colten.appleby@gmail.com>
2021-04-09 13:42:20 -0400

    commit tree f5e10c430c755a229329dc75a15fc8a1090f0df8

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+
+    const input = document.querySelectorAll('input')[0];
+
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+    });
+
 }
+
+
